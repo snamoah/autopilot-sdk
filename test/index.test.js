59dead34861819850f739242999ad7f0592a6499
Samuel Amoah <snamoah@gmail.com>
2017-08-30 22:26:02 +0000

    initial commit

---
+++ test/index.test.js
@@ -0,0 +1,38 @@
+import Autopilot from '../lib';
+import utils from '../lib/utils';
+import sinon from 'sinon';
+import test from 'tape';
+
+test('Autopilot', t => {
+  t.plan(5);
+
+  const autopilot = new Autopilot('sample api key');
+  t.ok(autopilot instanceof Autopilot, 'creates Autopilot instance');
+  t.ok(autopilot.apiKey === undefined, 'apiKey is not publicly accessible');
+
+  t.comment('Contacts');
+  t.ok(autopilot.contacts instanceof Object, 'contacts instance method exists');
+  const requestStub = sinon.stub(utils, 'request');
+  requestStub.returns(Promise.resolve());
+
+  const contactData = {
+    email: 'test@test.com',
+    'Full Name': 'John Doe',
+    bar: false,
+    age: 34,
+  };
+
+  let res = autopilot.contacts.save(contactData);
+  t.ok(res instanceof Promise, 'Saves contact data on Autopilot');
+
+  const falsyData = {
+    banana() {},
+    beach: {
+      fresh: 1,
+    },
+  };
+  const fn = () => autopilot.contacts.save(falsyData);
+  t.throws(fn, 'throws with invalid data');
+
+  requestStub.restore();
+});
---
+++ test/utils.test.js
@@ -0,0 +1,57 @@
+import {isDate, isFloat, hyphenate, parse} from '../lib/utils';
+import test from 'tape';
+
+test('isDate', t => {
+  t.plan(2);
+  const date = new Date();
+
+  // should be true if it is a valid date
+  t.ok(isDate(date), 'returns true for valid date');
+  t.notOk(isDate('foobar'), 'returns false for invalid date');
+});
+
+test('isFloat', t => {
+  t.plan(3);
+
+  t.ok(isFloat(2.3), 'returns true for valid floating point number');
+  t.notOk(isFloat('2.3'), 'returns false for floating point string numbers');
+  t.notOk(isFloat(2), 'returns false for integer values');
+});
+
+test('hyphenate', t => {
+  t.plan(2);
+
+  t.equal(hyphenate('New Store'), 'New--Store', 'returns hyphenated string');
+  t.equal(
+    hyphenate('   New Store   '),
+    'New--Store',
+    'trims string and returns hyphenated string',
+  );
+});
+
+test('parse', t => {
+  t.plan(2);
+
+  const obj = {
+    'First Name': 'Jon',
+    'Last Name': 'Doe',
+    age: 34,
+    gender: 1,
+    married: false,
+  };
+
+  t.deepEqual(
+    parse(obj),
+    {
+      'string--First--Name': 'Jon',
+      'string--Last--Name': 'Doe',
+      'integer--age': 34,
+      'integer--gender': 1,
+      'boolean--married': false,
+    },
+    'asserts true for valid object properties',
+  );
+
+  const fn = () => parse({foo: {bar: 1}});
+  t.throws(fn, 'throws error for nested object');
+});
